045f59553b6baae2abc29dc4b4a3d2497a4baac3
Florian Kogon <F.Kogon@gmail.com>
2021-01-28 00:09:11 +0100

    add-method-for-sign-in

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,14 +1,15 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 abstract class AuthBase {
   Stream<User> authStateChanges();
   User get currentUser;
+  Future<User> signInWithGoogle();
   Future<User> signInAnonymously();
   Future<void> signOut();
 }
 
 class Auth implements AuthBase {
-
   final _firebaseAuth = FirebaseAuth.instance;
 
   @override
@@ -16,6 +17,34 @@ class Auth implements AuthBase {
 
   @override
   User get currentUser => _firebaseAuth.currentUser;
+
+  @override
+  Future<User> signInWithGoogle() async {
+    final googleSignIn = GoogleSignIn();
+    final googleUser = await googleSignIn.signIn();
+    if (googleUser != null) {
+      final googleAuth = await googleUser.authentication;
+      if (googleAuth.idToken != null) {
+        final userCredential = await _firebaseAuth.signInWithCredential(
+          GoogleAuthProvider.credential(
+            idToken: googleAuth.idToken,
+            accessToken: googleAuth.accessToken,
+          ),
+        );
+        return userCredential.user;
+      } else {
+        throw FirebaseAuthException(
+          message: 'Missing Google ID Token',
+          code: 'ERROR_MISSING_ID_TOKEN',
+        );
+      }
+    } else {
+      throw FirebaseAuthException(
+        message: 'Sing in aborted by user',
+        code: 'ERROR_ABORTED_BY_USER',
+      );
+    }
+  }
 
   @override
   Future<User> signInAnonymously() async {
@@ -27,5 +56,4 @@ class Auth implements AuthBase {
   Future<void> signOut() async {
     await _firebaseAuth.signOut();
   }
-
 }
