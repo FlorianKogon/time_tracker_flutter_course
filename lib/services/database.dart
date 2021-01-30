f287337010eb821dfa2d8b2be0fdc7275a9344bc
Florian Kogon <F.Kogon@gmail.com>
2021-01-30 23:55:32 +0100

    read jobs method added

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,9 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:time_tracker_flutter_course/app/home/models/job.dart';
+import 'package:time_tracker_flutter_course/services/api_path.dart';
 
 abstract class Database {
-  Future<void> createJob(Map<String, dynamic> jobData);
-
+  Future<void> createJob(Job job);
+  Stream<List<Job>> jobsStream();
 }
 
 class FirestoreDatabase implements Database {
@@ -11,11 +13,24 @@ class FirestoreDatabase implements Database {
 
   final String uid;
 
-  @override
-  Future<void> createJob(Map<String, dynamic> jobData) async {
-    final path = '/users/$uid/jobs/job_abc';
-    final documentReference = FirebaseFirestore.instance.doc(path);
-    await documentReference.set(jobData);
+  Future<void> createJob(Job job) => _setData(
+        APIPath.job(uid, 'jobId'),
+        job.toMap(),
+      );
+
+  Stream<List<Job>> jobsStream() {
+    final path = APIPath.jobs(uid);
+    final reference = FirebaseFirestore.instance.collection(path);
+    final snapshots = reference.snapshots();
+    return snapshots.map((event) => event.docs.map((e) {
+          final data = e.data();
+          return data != null ? Job(name: data['name'], ratePerHour: data['ratePerHour']) : null;
+        }));
   }
 
+  Future<void> _setData(String path, Map<String, dynamic> data) async {
+    final reference = FirebaseFirestore.instance.doc(path);
+    print('$path: $data');
+    await reference.set(data);
+  }
 }
